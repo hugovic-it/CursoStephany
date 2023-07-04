c5c9f831cff4b4b9ca804669ebb3b1f2b1e4c409
Hugo Victor <hugovic.it@gmail.com>
2023-07-04 10:47:13 -0300

    28. Endpoints + Status code

--- Program.cs
+++ Program.cs
@@ -7,21 +7,27 @@ var app = builder.Build();
 
 app.MapPost("/products", (Product product) => {
     ProductRepository.Add(product);
+    return Results.Created("/products/" + product.Code, product.Code);
 });
 
 app.MapGet("/products/{code}", ([FromRoute]string code) => {
     var product = ProductRepository.GetBy(code);
-    return product;
+    if(product != null)
+        return Results.Ok(product);
+    return Results.NotFound();
+
 });
 
 app.MapPut("/products", (Product product) => {
     var productSaved = ProductRepository.GetBy(product.Code);
     productSaved.Name = product.Name;
+    return Results.Ok();
 });
 
 app.MapDelete("/products/{code}",([FromRoute] string code) => {
     var productSaved = ProductRepository.GetBy(code);
     ProductRepository.Remove(productSaved);
+    return Results.Ok();
 });
 app.Run();
 
