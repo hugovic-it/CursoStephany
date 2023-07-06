dc70b0cc476e903bcb5bc0b7ddda994c9fea589c
Hugo Victor <hugovic.it@gmail.com>
2023-07-06 11:41:24 -0300

    30. Arquivo de configuracao

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
 
 app.MapPost("/products", (Product product) => {
     ProductRepository.Add(product);
@@ -29,16 +31,22 @@ app.MapDelete("/products/{code}",([FromRoute] string code) => {
     ProductRepository.Remove(productSaved);
     return Results.Ok();
 });
+
+app.MapGet("/configuration/database",(IConfiguration configuration) =>{
+    return Results.Ok(configuration["database:connection"] + ":" + configuration["database:port"]);
+});
 app.Run();
 
 
 public static class ProductRepository {
-    public static List<Product> Products { get; set; }
+    public static List<Product> Products { get; set; } = Products = new List<Product>();
+
+    public static void Init(IConfiguration configuration) {
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
 
     public static void Add(Product product){
-        if(Products is null){
-            Products = new List<Product>();
-        }
         Products.Add(product);
     }
 
