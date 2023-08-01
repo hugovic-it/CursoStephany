e8f6575827f5f5953bd1c972e840eceff152a061
Hugo Victor <hugovic.it@gmail.com>
2023-08-01 10:34:29 -0300

    42. Revertendo migration do banco de dados

--- Program.cs
+++ Program.cs
@@ -72,6 +72,7 @@ public class Product{
     public int Id { get; set; }
     public string? Code { get; set; }
     public string? Name { get; set; }
+    public string? Description { get; set; }
 }
 
 public class ApplicationDbContext : DbContext {
